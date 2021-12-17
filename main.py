90dad47cf142d797ee3c12c3ffb6067ad16b24fb
fatinmidage <fg1048596@live.com>
2021-12-17 17:28:29 +0800

    修改data

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import urllib.parse
 import json
 
 
+def save_file(filename, content):
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.write(str(content))
+
+
 def get_huizhou_houses_address():
+    '''
     data = {
         'advanced_search': '"[{\"filter\":\"and\",\"filed\":\"PRESELL.REGIONCODE\",\"code\":\"like\",\"value\":\"441302\",\"type\":\"undefined\"},{\"filter\":\"and\",\"filed\":\"PRESELL.PROJECT\",\"code\":\"like\",\"value\":\"明昱\",\"type\":\"undefined\"}]"',
         'limit': '200', 'offset': '0', 'order': "desc", 'pageNumber': '1', 'search_filed': "[]",
         'sort': "presell.INDATE", '_token': ""}
     data = urllib.parse.urlencode(data).encode('utf-8')
+    '''
 
     response = urllib.request.urlopen('http://113.106.199.150:9086/web_homepage/houseList')
     content = response.read().decode('utf-8')
@@ -18,8 +25,7 @@ def get_huizhou_houses_address():
         if each_house['PROJECT'] not in house_data.keys():
             house_data[each_house['PROJECT']] = each_house['ADDRESS']
 
-    with open('houses_data.txt', mode='w', encoding='utf-8') as b:
-        b.write(str(house_data))
+    save_file('houses_data.txt', house_data)
 
 
 # 按间距中的绿色按钮以运行脚本。
