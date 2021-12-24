89075c770f80d60c24d521da1711d3070d0a2386
fatinmidage <fg1048596@live.com>
2021-12-24 15:05:50 +0800

    推导式

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import urllib.request
-import urllib.parse
 import json
 
 
@@ -13,10 +12,12 @@ def get_huizhou_houses_address():
     response = urllib.request.urlopen('http://113.106.199.150:9086/web_homepage/houseList')
     content = response.read().decode('utf-8')
     result = json.loads(content)
+
+    # dict_houses包含了所有的房屋信息，有重复项
+    dict_houses = result['rows']
     house_data = {}
-    for each_house in result['rows']:
-        if each_house['PROJECT'] not in house_data.keys():
-            house_data[each_house['PROJECT']] = each_house['ADDRESS']
+    # house_data只包含房屋名称和地址，且无重复项
+    house_data = {each_house['PROJECT']:each_house['ADDRESS'] for each_house in dict_houses}
 
     save_file('houses_data.txt', house_data)
 
